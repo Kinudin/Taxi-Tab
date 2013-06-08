41d0cf50d714dd9107ee500c2e068d80fa25eb25
Eric Cross <crosse88@gmail.com>
2013-06-08 11:42:11 -0700

    Update calculator logic

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 Taxi Tab is the final project for Interface Design II.
 The purpose is to create a responsive iPad app using `@media only screen` queries.
 
+##Calculate Screen
+Takes the number of miles, multiplying it by $3/mile. Plus the base sittingFee. Then it divides the total number of people in your party to get what each person should pay.
+
 Also as a side project, I'm figuring out just how the heck GitHub works.
--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,29 @@ function calculate(){
     	fee = 0;
     	total = numMiles * perMileFee;
     	numPeople = document.getElementById('numPeople').value;
-    	// eachPaysFunc =  total / numPeople;
 	if (numPeople > 4 && numPeople < 10){
 		document.getElementById('fees').innerHTML = 'Fees: $12';
 		fee = 12;
 		total = total + fee;
-		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+eachPaysFunc;
+		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+ calculateFee(total,numPeople,fee);
 	} else if(numPeople > 10){
 		document.getElementById('fees').innerHTML = 'Fees: $20';
 		fee = 20;
 		total = total + fee;
-		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+eachPaysFunc;
+		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+ calculateFee(total,numPeople,fee);
 	} else if (numPeople <= 4){
 		document.getElementById('fees').innerHTML = 'Fees: $3.50';
 		fee = 3.50;
 		total = total +  fee;
-		// var eachPaysFunc = calculateFee(total, numPeople, fee);
 		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+ calculateFee(total,numPeople,fee);
 	}
 
-	document.getElementById('theTotal').innerHTML = 'Total: $' + total;	
+	document.getElementById('theTotal').innerHTML = 'Total: $' + total.toFixed(2);	
 }
 
 function calculateFee (total, numPeople, fee) {
 	alert('Total: ' + total + 'Fee: ' + fee);
-	eachPaysFunc = (total + fee)/numPeople;
+	eachPaysFunc = total/numPeople;
+	eachPaysFunc = eachPaysFunc.toFixed(2);
 	return eachPaysFunc;
 }
