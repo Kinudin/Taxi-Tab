09df5c01c70d6b9cf11076a780c04cb1ce14ac31
Eric Cross <ericcross@Erics-iMac.local>
2013-06-08 10:24:33 -0700

    Calculator Logic update (include args…NOT WORKING)
    Including arguments. Logic doesn't include the 'sitting fee' for the
    amount of people entering the taxi.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 function calculate(){
 	var numMiles = document.getElementById('numMiles').value;
+    	perMileFee = 3;
+    	fee = 0;
+    	total = numMiles * perMileFee;
     	numPeople = document.getElementById('numPeople').value;
-    	total = numMiles * numPeople;
+    	// eachPaysFunc =  total / numPeople;
 	if (numPeople > 4 && numPeople < 10){
 		document.getElementById('fees').innerHTML = 'Fees: $12';
-		total = total+ 12;
-	}else if(numPeople > 10){
+		fee = 12;
+		total = total + fee;
+		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+eachPaysFunc;
+	} else if(numPeople > 10){
 		document.getElementById('fees').innerHTML = 'Fees: $20';
-		total = total + 20;
-	}
-	 else if (numPeople > 10){
-		total = total + 20;
-	} else if (numPeople < 4){
-		total = total +  3.50;
+		fee = 20;
+		total = total + fee;
+		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+eachPaysFunc;
+	} else if (numPeople <= 4){
+		document.getElementById('fees').innerHTML = 'Fees: $3.50';
+		fee = 3.50;
+		total = total +  fee;
+		// var eachPaysFunc = calculateFee(total, numPeople, fee);
+		document.getElementById('eachPays').innerHTML = 'Everyone pays: $'+ calculateFee(total,numPeople,fee);
 	}
 
 	document.getElementById('theTotal').innerHTML = 'Total: $' + total;	
 }
 
-
+function calculateFee (total, numPeople, fee) {
+	alert('Total: ' + total + 'Fee: ' + fee);
+	eachPaysFunc = (total + fee)/numPeople;
+	return eachPaysFunc;
+}
